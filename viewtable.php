948778f9e1cf711df771e528de11b6c5af5229f9
Jacky26 <44333748+Jacky26@users.noreply.github.com>
2020-11-25 12:09:56 +0800

    Update viewtable.php

--- viewtable.php
+++ viewtable.php
@@ -21,16 +21,7 @@ if (!$conn)
  die(sqlsrv_errors());
  }
 
- while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
- {
- echo "<tr>";
- echo "<td>". $row['restaurant_id'] . "</td>";
- echo "<td>". $row['restaurant_name'] ."</td>";
- echo "<td>". $row['restaurant_address'] . "</td>";
- echo "<td>". $row['restaurant_phone'] . "</td>";
- echo "</tr>";
- }
- sqlsrv_free_stmt($getResults);
+
 ?>
 
 <html>
@@ -48,12 +39,18 @@ if (!$conn)
  <th>Address</th>
  <th>Phone</th>
  </tr>
- <tr>
- <th>0</th>
- <th>Subway Restaurant</th>
- <th>Menara Standard Chartered, TPM</th>
- <th>03-22441234</th>
- </tr>
+ <?php
+ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+ {
+ echo "<tr>";
+ echo "<td>". $row['restaurant_id'] . "</td>";
+ echo "<td>". $row['restaurant_name'] ."</td>";
+ echo "<td>". $row['restaurant_address'] . "</td>";
+ echo "<td>". $row['restaurant_phone'] . "</td>";
+ echo "</tr>";
+ }
+ sqlsrv_free_stmt($getResults);
+ ?>
  </table>
  </center>
  </body>
